372f2ac0435b01f9d6e0cb764acbe3190d4423ce
Kyle Gough <kyle.gough3@gmail.com>
2023-06-12 15:06:45 -0500

    Updated AdminController

--- StoreFront/StoreFront.UI.MVC/Controllers/AdministrationController.cs
+++ StoreFront/StoreFront.UI.MVC/Controllers/AdministrationController.cs
@@ -1,9 +1,9 @@
-﻿using [YourProjectName].UI.MVC.Models;
+﻿using StoreFront.UI.MVC.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-namespace [YourProjectName].UI.MVC.Controllers//Update namespace
+namespace StoreFront.UI.MVC.Controllers//Update namespace
 {
     [Authorize(Roles = "Admin")]
     public class AdministrationController : Controller
